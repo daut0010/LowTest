970a9fed97c12ba7803cef770faebda8104b79c3
Kadir Polat <kadirpolat8210@hotmail.com>
2022-10-05 15:13:33 +0200

    test

---
+++ SimpleTCP/dk/via/jpe/simpletcp/SimpleTCPConfig.java
@@ -0,0 +1,13 @@
+/*
+ * 08.09.2019 package name updated
+ * 28.02.2011 Original version
+ */
+ 
+ 
+package dk.via.jpe.simpletcp;
+
+
+public class SimpleTCPConfig
+{
+	public static final int PORTNO = 4711;
+}
